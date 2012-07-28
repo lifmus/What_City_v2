f0969753ee88b63e2ab3a2948013b6b9a084114a
Andrew Lin <f50ajl@gmail.com>
2012-07-28 14:18:00 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhatCity::Application.config.secret_token = 'e7f705d3f1224114129842ecd2b65d92244d540e881e131ea5a6db2d5a9d9eda74108562a43a45f503897d62096eb53669091be925f0b9fde783d6dadf77ae22'
